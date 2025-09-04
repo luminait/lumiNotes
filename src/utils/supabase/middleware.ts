4afe1b3eef8ce970056de59c855872b12fcdcbe6
shahasad78 <shahasad78@gmail.com>
2025-09-04 14:53:44 -0700

    Updated Supabase middleware.ts to handle re-authorization of session cookies.

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,106 +1,52 @@
-import {createServerClient} from '@supabase/ssr'
-import {NextResponse, type NextRequest} from 'next/server'
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { type NextRequest, NextResponse } from 'next/server'
 
 export async function updateSession(request: NextRequest) {
-    let supabaseResponse = NextResponse.next({
-        request,
-    })
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  })
 
-    console.log("middleware updateSession called");
-
-    const supabase = createServerClient(
-        process.env.SUPABASE_URL!,
-        process.env.SUPABASE_ANON_KEY!,
-        {
-            cookies: {
-                getAll() {
-                    return request.cookies.getAll()
-                },
-                setAll(cookiesToSet) {
-                    cookiesToSet.forEach(({name, value, options}) => request.cookies.set(name, value))
-                    supabaseResponse = NextResponse.next({
-                        request,
-                    })
-                    cookiesToSet.forEach(({name, value, options}) =>
-                        supabaseResponse.cookies.set(name, value, options)
-                    )
-                },
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
             },
-        }
-    )
-
-    // If the user is logged in, the can't go to the login or signup page
-    const isAuthRoute =
-        request.nextUrl.pathname.startsWith('/login') ||
-        request.nextUrl.pathname.startsWith('/signup');
-
-    if (isAuthRoute) {
-        const {
-            data: {user},
-        } = await supabase.auth.getUser()
-
-        if (user) {
-            return NextResponse.redirect(new URL('/', process.env.NEXT_PUBLIC_BASE_URL!))
-        }
+          })
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+        },
+        remove(name: string, options: CookieOptions) {
+          request.cookies.set({ name, value: '', ...options })
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          })
+          response.cookies.set({ name, value: '', ...options })
+        },
+      },
     }
+  )
 
-    // If the URL has no noteId, try to go the user's most recent note.
-    // If the user has no notes, redirect to the create note page.
-    const {searchParams, pathname} = new URL(request.url);
-    if (!searchParams.get('noteId') && pathname === '/') {
-        const {
-            data: {user},
-        } = await supabase.auth.getUser()
+  await supabase.auth.getUser()
 
-
-        if (user) {
-            const {newestNoteId} =
-                await fetch(`${process.env.NEXT_PUBLIC_BASE_URL!}/api/fetch-newest-note?userId=${user.id}`)
-                .then((res) => res.json());
-
-            if (newestNoteId) {
-                const url = request.nextUrl.clone();
-                url.searchParams.set('noteId', newestNoteId);
-                return NextResponse.redirect(url);
-            } else {
-                const noteId =
-                    await fetch(`${process.env.NEXT_PUBLIC_BASE_URL!}/api/create-new-note?userId=${user.id}`, {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                    }).then((res) => res.json());
-                const url = request.nextUrl.clone();
-                url.searchParams.set('noteId', noteId);
-                return NextResponse.redirect(url);
-
-            }
-        }
-    }
-
-    // Do not run code between createServerClient and
-    // supabase.auth.getUser(). A simple mistake could make it very hard to debug
-    // issues with users being randomly logged out.
-
-    // IMPORTANT: DO NOT REMOVE auth.getUser()
-
-    const {
-        data: {user},
-    } = await supabase.auth.getUser()
-
-
-    // IMPORTANT: You *must* return the supabaseResponse object as it is.
-    // If you're creating a new response object with NextResponse.next() make sure to:
-    // 1. Pass the request in it, like so:
-    //    const myNewResponse = NextResponse.next({ request })
-    // 2. Copy over the cookies, like so:
-    //    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
-    // 3. Change the myNewResponse object to fit your needs, but avoid changing
-    //    the cookies!
-    // 4. Finally:
-    //    return myNewResponse
-    // If this is not done, you may be causing the browser and server to go out
-    // of sync and terminate the user's session prematurely!
-
-    return supabaseResponse
+  return response
 }
